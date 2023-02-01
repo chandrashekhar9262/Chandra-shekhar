68815e2ffe42dde88905d5dc7f31c13f074df88b
chandrashekhar9262 <93523466+chandrashekhar9262@users.noreply.github.com>
2023-02-01 22:49:50 +0530

    for loop.cpp

---
+++ print n number using for loop.cpp
@@ -0,0 +1,15 @@
+//Write a program to print first n number using for loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number of element :";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" ";
+        
+    }
+    return 0;
+}
