6481adaa40f8950f0a5b2452ef30b856e8008bf1
chandrashekhar9262 <93523466+chandrashekhar9262@users.noreply.github.com>
2023-02-01 21:25:40 +0530

    Create ifelse.cpp

---
+++ ifelse.cpp
@@ -0,0 +1,21 @@
+//Write a program to check whether a number is Even or Odd
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter a number :";
+    cin>>a;
+    if(a%2==0)
+    {
+        cout<<"Even number";
+    }
+    else
+    {
+        cout<<"Odd number";
+    }
+
+    return 0;
+}
